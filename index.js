70aaebd1439d9e64a208345ce1a3fe69ce6672ad
Ivan Chaplinskyi <chaplinskyi.ivan@gmail.com>
2024-10-14 23:19:05 +0300

    Revert "add-vercel"
    This reverts commit 32cb0f2aa5eb5b13a61cbae4f8812d5dd5407022.

--- index.js
+++ index.js
@@ -7,8 +7,4 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
 
-// app.use('/api/v1/', tourRouter);
-
-app.get('/', (req, res) => res.json({ title: 'Telegram Bot' }));
-
 module.exports = app;
