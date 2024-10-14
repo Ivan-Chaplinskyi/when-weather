6ccb705305c5f853275fbbcbcb68200a883f8386
Ivan Chaplinskyi <chaplinskyi.ivan@gmail.com>
2024-10-14 11:39:37 +0300

    setInterval-for-server

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const db = process.env.DATABASE.replace(
 // eslint-disable-next-line no-console
 mongoose.connect(db).then(() => console.log('DB connection successfull!'));
 
+// eslint-disable-next-line no-console
+setInterval(() => console.log('Server working...'), 30000);
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () =>
